fe1ed1e41d04f2b5b76bc23c1363616d607ee222
xijie12 <1058805781@qq.com>
2019-12-04 11:07:40 +0800

    03_添加区块链结构及创世块

--- main.go
+++ main.go
@@ -43,13 +43,31 @@ func (block *Block) SetHash() {
 	block.Hash = res*/
 }
 //4.引入区块链
+type BlockChain struct {
+	//定义一个区块链数组
+	blocks []*Block
+}
+//5.定义一个区块链
+func NewBlockChain() *BlockChain {
+	genesisBlock := GenesisBlock()
+	return &BlockChain{
+		blocks: []*Block{genesisBlock},
+	}
+}
+//定义一个创世块
+func GenesisBlock() *Block{
+	return NewBlock("Go一期创世块。", []byte{})
+}
 //5.添加区块
 //6.重构代码
 
 func main(){
-	block := NewBlock("转让一枚比特币", []byte{})
+	bc := NewBlockChain()
 
-	fmt.Printf("前区块哈希值： %x\n", block.PrevHash)
-	fmt.Printf("当前区块哈希值： %x\n", block.Hash)
-	fmt.Printf("区块数据： %s\n", block.Data)
+	for i, block := range bc.blocks {
+		fmt.Printf("=========当前区块高度: %d =========\n", i)
+		fmt.Printf("前区块哈希值： %x\n", block.PrevHash)
+		fmt.Printf("当前区块哈希值： %x\n", block.Hash)
+		fmt.Printf("区块数据： %s\n", block.Data)
+	}
 }
