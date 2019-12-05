844b9e408b75ccf53465b3562de50eb3f3540ff9
xijie12 <1058805781@qq.com>
2019-12-05 16:48:24 +0800

    09_工作量证明

--- proofOfWork.go
+++ proofOfWork.go
@@ -15,6 +15,7 @@ func NewProofOfWork(block *Block) *ProofOfWork{
 	targeStr := "0001000000000000000000000000000000000000000000000000000000000000"
 	//引入辅助变量
 	tmpInt := big.Int{}
+	//将难度值赋值给big.Int,使用16进制格式
 	tmpInt.SetString(targeStr,16)
 	pow.target = &tmpInt
 	return &pow
