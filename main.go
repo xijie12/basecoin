07c718f16adc0027c555c062cf6ebf01548f7490
xijie12 <1058805781@qq.com>
2019-12-05 17:02:26 +0800

    让

--- main.go
+++ main.go
@@ -4,7 +4,7 @@ import "fmt"
 
 func main(){
 	bc := NewBlockChain()
-	bc.AddBlock("A向B转出50枚比特币！")
+	bc.AddBlock("A向B转让50枚比特币！")
 	for i, block := range bc.blocks {
 		fmt.Printf("=========当前区块高度: %d =========\n", i)
 		fmt.Printf("前区块哈希值： %x\n", block.PrevHash)
